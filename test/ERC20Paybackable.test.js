5fc2db7c2ff832e3a0d2662052721be32d439680
Sutee Sudprasert <sutee.s@gmail.com>
2021-07-16 12:39:05 +0700

    update unit test

--- test/ERC20Paybackable.test.js
+++ test/ERC20Paybackable.test.js
@@ -30,11 +30,11 @@ describe("ERC20Paybackable", () => {
       await barToken.transfer(fooToken.address, toWei("1000"), { from: bob });
       await fooToken.payback(bob, barToken.address, toWei("1000"));
       const bobBalance = BigNumber(await barToken.balanceOf(bob));
+      expect(bobBalance).to.be.bignumber.equal(BigNumber(toWei("10000")));
 
       await fooToken.transfer(fooToken.address, toWei("1000"), { from: marry });
       await fooToken.payback(marry, fooToken.address, toWei("1000"));
       const marryBalance = BigNumber(await fooToken.balanceOf(marry));
-
       expect(marryBalance).to.be.bignumber.equal(BigNumber(toWei("10000")));
     });
   });
